db4a7ab5371b370de83d68069f0094cb8cd839b9
KHardik1698 <hardik.kundalia@contentstack.com>
2020-12-31 15:12:12 +0530

    Added CORS dependency to allow request from other sources

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const dotenv = require("dotenv");
 dotenv.config({ path: "./config.env" });
 const blogRoute = require("./routes/blogRoutes");
 
+app.use(cors());
 app.use("/blogs", blogRoute);
 
 app.listen(process.env.PORT, () => {
